bd6e294f847eded79cb51fb39c6845fe5d47e233
Raul <raulamsales@gmail.com>
2021-10-13 15:32:05 -0300

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Raul Sales on 13/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
